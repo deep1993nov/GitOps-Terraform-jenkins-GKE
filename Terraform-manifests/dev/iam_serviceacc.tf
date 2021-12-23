57009b405879c621c11c7a5e0e71a65ceca45b73
Subarno Chakraborty <subarnochakraborty@Subarnos-MacBook-Pro.local>
2021-12-23 11:05:57 +0530

    Create test service acc..

--- Terraform-manifests/dev/iam_serviceacc.tf
+++ Terraform-manifests/dev/iam_serviceacc.tf
@@ -3,7 +3,7 @@ resource "google_service_account" "sa-name" {
   display_name = "terraform-created-srv2"
 }
 
-resource "google_service_account" "sa-name" {
+resource "google_service_account" "sa-name1" {
   account_id = "terraform-created-srv3"
   display_name = "terraform-created-srv3"
 }
