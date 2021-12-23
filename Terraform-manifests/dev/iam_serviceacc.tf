a74d1d455e217be082036f61021f3264da5891d4
Subarno Chakraborty <subarnochakraborty@Subarnos-MacBook-Pro.local>
2021-12-23 13:34:35 +0530

    Remove test service acc..

--- Terraform-manifests/dev/iam_serviceacc.tf
+++ Terraform-manifests/dev/iam_serviceacc.tf
@@ -3,8 +3,5 @@ resource "google_service_account" "sa-name" {
   display_name = "terraform-created-srv2"
 }
 
-resource "google_service_account" "sa-name2" {
-  account_id = "terraform-created-srv3"
-  display_name = "terraform-created-srv3"
-}
+
 
