f50dc74e2124a939d74313d6bef0df63f26e7990
Subarno Chakraborty <subarnochakraborty@Subarnos-MacBook-Pro.local>
2021-12-24 11:19:58 +0530

    new Commit

--- Terraform-manifests/dev/iam_serviceacc.tf
+++ Terraform-manifests/dev/iam_serviceacc.tf
@@ -4,8 +4,8 @@ resource "google_service_account" "sa-name" {
 }
 
 resource "google_service_account" "new-SRV" {
-  account_id = "terraform-created-srv3"
-  display_name = "terraform-created-srv3"
+  account_id = "terraform-created-srv4"
+  display_name = "terraform-created-srv4"
 }
 
 
